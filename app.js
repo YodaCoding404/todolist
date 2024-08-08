97e968028d5f652a319636318b11e3011328ccc7
Spades <thenightking011@gmail.com>
2024-08-08 08:27:43 +0200

    IP public and view/public folder shared

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
-
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Database
 const databaseName = "todolistDB";
